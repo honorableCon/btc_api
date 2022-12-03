adfde6beefde9e15d7a6b2108d88a3bf4615cd4b
Honorable Con <honorablecon@gmail.com>
2022-12-03 05:44:41 +0000

    adding ussd routing

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const { sendBitcoin } = require("./service/transactions");
 const { createWallet } = require("./service/wallet");
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/wallet", (req, res) => {
   const { network } = req.body;
