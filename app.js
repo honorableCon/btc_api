0e267d47d8912855675d1707b96e7c2a1dfb4e38
Honorable Con <honorablecon@gmail.com>
2022-12-03 05:29:40 +0000

    adding ussd route

--- app.js
+++ app.js
@@ -23,4 +23,26 @@ app.get("/", (req, res) => {
   res.send("Welcome to SunuWallet !");
 });
 
+app.post("/ussd", (req, res) => {
+  const { sessionId, serviceCode, phoneNumber, text } = req.body;
+
+  let response = "";
+
+  if (text == "") {
+    response = `CON What would you like to check
+        1. My account
+        2. My phone number`;
+  } else if (text == "1") {
+    response = `CON Choose account information you want to view
+        1. Account number`;
+  } else if (text == "2") {
+    response = `END Your phone number is ${phoneNumber}`;
+  } else if (text == "1*1") {
+    const accountNumber = "ACC100101";
+    response = `END Your account number is ${accountNumber}`;
+  }
+  res.set("Content-Type: text/plain");
+  res.send(response);
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
