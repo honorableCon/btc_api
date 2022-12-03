f14d0f1a423d7a0c9a6660a8a302e7a732a4c18c
Honorable Con <honorablecon@gmail.com>
2022-12-03 00:40:43 +0000

    initital commit

--- api/app.js
+++ api/app.js
@@ -1,15 +1,25 @@
-require('dotenv').config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const authRouter = require('./routes/auth');
+const { sendBitcoin } = require("./service/transactions");
+const { createWallet } = require("./service/wallet");
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use('/auth', authRouter);
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.post("/wallet", (req, res) => {
+  const wallet = createWallet();
+  res.json(wallet);
+});
+
+app.post("/transactions", async (req, res) => {
+  const { sender, receiver, privateKey, amount } = req.body;
+  const txn = await sendBitcoin(sender, receiver, amount, privateKey);
+  res.json(txn);
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to SunuWallet!");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
